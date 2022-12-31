4ca2ef348a26744fbb886b216b7de4b3cb5d50a1
Geras4323 <german432yo@gmail.com>
2022-12-30 22:49:35 -0300

    [FIX] Fixed Analytics import

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles/globals.css';
 
-import { Analytics } from '@vercel/analytics';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
