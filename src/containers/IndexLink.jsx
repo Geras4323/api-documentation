2d48a9eb950fab32c1796e3ed707385d7f957a99
Geras4323 <german432yo@gmail.com>
2023-01-01 15:33:28 -0300

    [MOD] Modified IndexOption target to _blank

--- src/containers/IndexLink.jsx
+++ src/containers/IndexLink.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 function IndexLink({ link, title, description }) {
   return (
-    <Link href={link} className='w-full h-full'>
+    <Link href={link} className='w-full h-full' target='_blank'>
       <div className='w-full h-full bg-black bg-opacity-40 rounded-xl border   hover:scale-105 transition-all duration-100'>
         <p className='glow h-auto py-1 px-4 text-lg rounded-t-xl bg-black bg-opacity-25'>{title}</p>
         <p className='p-2 px-4 text-md'>{description}</p>
