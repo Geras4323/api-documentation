406be555187edb1f4453d450f4507c0da49e9eb4
Geras4323 <german432yo@gmail.com>
2023-02-12 19:19:25 -0300

    [FIX] Fix axios baseUrl

--- src/config/axiosConnection.ts
+++ src/config/axiosConnection.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-  // baseURL: 'https://gerasdatabase.up.railway.app/api/v1'
-  baseURL: 'http://localhost:5000/api/v1'
+  baseURL: 'https://gerasdatabase.up.railway.app/api/v1'
+  // baseURL: 'http://localhost:5000/api/v1'
 })
 
 export { api };
