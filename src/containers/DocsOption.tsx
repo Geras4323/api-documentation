2f2824fb503b1aad676f8046d52fcd8739b32202
Geras4323 <german432yo@gmail.com>
2023-03-22 19:53:50 -0300

    [MOD] /docs' options are now clickable in their enterity

--- src/containers/DocsOption.tsx
+++ src/containers/DocsOption.tsx
@@ -10,34 +10,33 @@ interface IOptions {
 
 function DocsOption({ title, image, path }: IOptions) {
   return (
-    <div className='group
-      w-[244px] h-[324px] bg-gray-200 relative flex justify-center items-center rounded-lg overflow-hidden
-      hover:scale-105 transition-all duration-200
-      '
-    >
+    <Link href={`/docs/${path}`}>
+      <div className='group
+        w-[244px] h-[324px] bg-gray-200 relative flex justify-center items-center rounded-lg overflow-hidden
+        hover:scale-105 transition-all duration-200
+        '
+      >
 
-      <div className={`w-128 h-128  conic-gradient  animate-spin-slow absolute`} />
+        <div className={`w-128 h-128  conic-gradient  animate-spin-slow absolute`} />
 
-      <section className='w-60 h-80 text-white  linear-gradient  rounded-lg absolute flex flex-col justify-between'>
-        <header className='w-full h-12 bg-black bg-opacity-30 flex justify-center items-center rounded-t-lg'>
-          <p className='text-2xl'>{title}</p>
-        </header>
+        <section className='w-60 h-80 text-white  linear-gradient  rounded-lg absolute flex flex-col justify-between'>
+          <header className='w-full h-12 bg-black bg-opacity-30 flex justify-center items-center rounded-t-lg'>
+            <p className='text-2xl'>{title}</p>
+          </header>
 
-        <section className='w-full flex justify-center'>
-          <img
-            src={`/${image}`}
-            className='w-44'
-          />
+          <section className='w-full flex justify-center'>
+            <img
+              src={`/${image}`}
+              className='w-44'
+            />
+          </section>
+
+          <div className='w-full h-12 bg-black bg-opacity-30 flex justify-center items-center rounded-b-lg   group-hover:bg-opacity-70   transition-all duration-100'>
+            <p className='flash text-lg'>View endpoints</p>
+          </div>
         </section>
-
-        <Link
-          href={`/docs/${path}`}
-          className='w-full h-12 bg-black bg-opacity-30 flex justify-center items-center rounded-b-lg   group-hover:bg-opacity-70   transition-all duration-100'
-        >
-          <p className='flash text-lg'>View endpoints</p>
-        </Link>
-      </section>
-    </div>
+      </div>
+    </Link>
   )
 }
 
